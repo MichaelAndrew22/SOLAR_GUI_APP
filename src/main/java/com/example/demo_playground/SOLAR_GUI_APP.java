47eba4f2726847a1bd039ccf183377534a8194fc
MichaelAndrew22 <fiat.money@protonmail.com>
2023-10-06 21:41:30 -0700

    INITIAL TEMPLATE SOLAR UI

--- src/main/java/com/example/demo_playground/SOLAR_GUI_APP.java
+++ src/main/java/com/example/demo_playground/SOLAR_GUI_APP.java
@@ -1,20 +1,37 @@
 package com.example.demo_playground;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
 public class SOLAR_GUI_APP extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         FXMLLoader fxmlLoader = new FXMLLoader(SOLAR_GUI_APP.class.getResource("SOLAR_GUI.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 420, 420);
+
+
+        stage.setOpacity(0.8);
+        StageStyle stageStyle = StageStyle.UNDECORATED;
+        stage.initStyle(stageStyle);
         stage.setTitle("SOLAR UI");
+        stage.setAlwaysOnTop(true);
+        stage.setResizable(true);
+
+
+
         stage.setScene(scene);
         stage.show();
+
     }
-    public static void main(String[] args) {
-        launch();
-    }
+
+
+
+
+    public static void main(String[] args) {launch();}
 }
--- src/main/java/com/example/demo_playground/SOLAR_GUI_CONTROLLER.java
+++ src/main/java/com/example/demo_playground/SOLAR_GUI_CONTROLLER.java
@@ -1,6 +1,7 @@
 package com.example.demo_playground;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 
@@ -11,9 +12,29 @@ public class SOLAR_GUI_CONTROLLER {
 
 
     @FXML
+    private Button button;
+
+
+    @FXML
     public void displayStats(ActionEvent actionEvent) {
         System.out.println("Displaying stats...");
         SOLAR_GUI_TEXT.setText("SOLAR_GUI_TEXT has been set!");
     }
+
+    @FXML
+    public void handleButtonAction(ActionEvent actionEvent) {
+        System.out.println("You clicked me!");
+
+
+        button.setStyle("-fx-background-color: #ffcc46;");
+
+
+        SOLAR_GUI_TEXT.setText("button response");
+        SOLAR_GUI_TEXT.setStyle("-fx-background-color: #7fcc70;");
+
+        //SOLAR_GUI_BAT.setText("bat");
+        //SOLAR_GUI_BAT.setStyle("-fx-background-color: #7fcc70;");
+    }
+
 
 }
